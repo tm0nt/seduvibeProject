245587f59f44fe7e7f62e501c48338b3ba7e24f1
tm0nt <tassio.leite14@gmail.com>
2024-01-16 14:51:05 -0300

    up

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,28 +1,23 @@
 <template>
   <v-bottom-navigation mode="shift" color="buttonNavColor" bg-color="navbarcolor">
-    <v-btn value="home">
+    <v-btn to="/home">
       <v-icon size="30">mdi-home</v-icon>
-
       <span>Home</span>
     </v-btn>
-
-    <v-btn value="notificacao">
+    <v-btn to="/notification">
       <v-icon size="30">mdi-heart</v-icon>
       <span>Notificações</span>
     </v-btn>
-    <v-btn>
+    <v-btn to="/search">
       <v-icon size="30">mdi-magnify</v-icon>
-
       <span>Pesquisar</span>
     </v-btn>
     <v-btn value="planos">
       <v-icon size="30">mdi-currency-usd</v-icon>
-
       <span>Plano</span>
     </v-btn>
     <v-btn to="/chat">
       <v-icon size="30">mdi-chat</v-icon>
-
       <span>Chat</span>
     </v-btn>
   </v-bottom-navigation>
