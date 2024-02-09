1a32013bc2146fabd15e61ea71fe5f31a7fb4b0e
tm0nt <tassio.leite14@gmail.com>
2024-02-09 13:55:00 -0300

    Update

--- middleware/check-auth.ts
+++
@@ -1,9 +0,0 @@
-import { useGlobalStore } from "~/store/index";
-
-export default defineNuxtRouteMiddleware((to, from) => {
-  const store = useGlobalStore();
-  const logado = store.logado;
-  if (logado === true) {
-    return navigateTo("/profile");
-  }
-});
