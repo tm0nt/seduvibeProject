eb481b16dafb803f8e8c3dcf7f59b635c6e745f0
tm0nt <tassio.leite14@gmail.com>
2024-02-19 10:29:49 -0300

    Update

--- server/api/webhooks.js
+++ server/api/webhooks.js
@@ -1,5 +1,9 @@
+import { idPayment } from "~/store/payment";
+
 export default defineEventHandler(async (event) => {
+  const idPaymentStore = idPayment();
   const body = await readBody(event);
+  idPaymentStore.setFetchPayment(event);
   console.log(body);
   return {
     success: true,
