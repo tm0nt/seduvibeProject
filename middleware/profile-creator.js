a4e33f7eee4949201d0e2d884e2f2d5267426a64
tm0nt <tassio.leite14@gmail.com>
2024-02-22 19:14:57 -0300

    Update

--- middleware/profile-creator.js
+++
@@ -1,8 +0,0 @@
-import { useIdStore } from "~/store/id";
-
-export default defineNuxtRouteMiddleware((to, from) => {
-    const storeId = useIdStore();
-    if(storeId.creatorId === 1){
-        return navigateTo("/profile/creator")
-    }
-  })
--- middleware/profile.js
+++
@@ -1,8 +0,0 @@
-import { useIdStore } from "~/store/id";
-
-export default defineNuxtRouteMiddleware((to, from) => {
-    const storeId = useIdStore();
-    if(storeId.creatorId === 0){
-        return navigateTo("/profile")
-    }
-  })
